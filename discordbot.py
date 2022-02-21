93533149d27e06af966ca6dde85b89ebcb3cd4c1
tonodrgr <100121846+tonodrgr@users.noreply.github.com>
2022-02-21 19:18:16 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -13,9 +13,25 @@ async def on_command_error(ctx, error):
 
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+async def vote(ctx, title, *select):
+  if len(select) > 10:
+    err = discord.Embed(title = "選択肢が多すぎます。", color = discord.Colour.red())
+    await ctx.send(embed = err)
+    return
 
+  emoji_list = ["1⃣", "2⃣", "3⃣", "4⃣", "5⃣", "6⃣", "7⃣", "8⃣", "9⃣", "🔟"]
+
+  # 縦並びの質問項目を生成
+  value = ""
+  for num in range(len(select)):
+    value += emoji_list[num] + select[num] + "\n"
+  # 全ての質問項目を1つのembed項目とする
+  embed = discord.Embed(title = value, color = discord.Colour.red())
+  
+  msg = await ctx.send("**" + title+ "**", embed = embed)
+  for i in range(len(select)):
+    await msg.add_reaction(emoji_list[i])
+  return
 
 token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
